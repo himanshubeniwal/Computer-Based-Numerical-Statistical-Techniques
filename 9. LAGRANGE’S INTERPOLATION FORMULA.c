03e9b495c30630fa4d9a5e92c0bdeef351a2ab1c
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2020-08-16 13:29:27 +0530

    Create 9. LAGRANGE’S INTERPOLATION FORMULA.c

---
+++ 9. LAGRANGE’S INTERPOLATION FORMULA.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define MAX 10
+void main()
+{
+    float x[MAX],y[MAX],k=0,z,nr,dr;                       int i,j,m;
+    printf("\n enter the range ");
+    scanf("%d",&m);
+    printf("\n enter the x value ");
+    for(i=0;i<m;i++)
+            scanf("%f",&x[i]);
+    printf("\n enter the y value ");
+    for(i=0;i<m;i++)
+            scanf("%f",&y[i]);
+    printf("\n enter value OF Z to be calculated ");
+            scanf("%f",&z);
+    for(i=0;i<m;i++)
+    {           nr=1;dr=1;
+                 for(j=0;j<m;j++)
+                 {    if (j!=i)
+                      {              nr=nr*(z-x[j]);
+                                     dr=dr*(x[i]-x[j]);
+                       }
+                  }
+                  k=k+((nr/dr)*y[i]);
+    }
+printf("\n final result=%f\n",k);
+getch();
+}
